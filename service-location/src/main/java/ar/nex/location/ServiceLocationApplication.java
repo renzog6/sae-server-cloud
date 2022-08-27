225ffdf62a7661ba509b6c8646950ecb2cd67f9d
renzog6 <renzog6@gmail.com>
2022-08-26 22:39:34 -0300

    Init service-location

---
+++ service-location/src/main/java/ar/nex/location/ServiceLocationApplication.java
@@ -0,0 +1,13 @@
+package ar.nex.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLocationApplication.class, args);
+	}
+
+}
---
+++ service-location/src/test/java/ar/nex/location/ServiceLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.nex.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
